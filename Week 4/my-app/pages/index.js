de76d5e02a509ee280ba17b5dabbcc256d7ca76b
Joe <cmksj1005@gmail.com>
2023-10-30 23:50:50 -0400

    Study React from beginning again

--- Week 4/my-app/pages/index.js
+++ Week 4/my-app/pages/index.js
@@ -1,13 +1,5 @@
 import Hello from '@/components/Hello';
-import Clock from '@/components/Clock';
-import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
-import styles from '@/styles/Home.module.css';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  // return <Hello fName="Jason" lName="Perez" />;
-  return <Clock locale="en-CA" />;
+  return <Hello />;
 }
