bc93f2631d10bc486304af4d246aa8c4db62f6a4
Joe <cmksj1005@gmail.com>
2023-11-14 23:25:58 -0500

    update _app.js for context

--- Layouts/my-app/pages/_app.js
+++ Layouts/my-app/pages/_app.js
@@ -1,9 +1,21 @@
 import '@/styles/globals.css';
-import { useState } from 'react';
+import { useState, createContext } from 'react';
+
+export const CountContext = createContext();
+export const SetCountContext = createContext();
 
 function MyApp({ Component, pageProps }) {
-  const [count, setCount] = useState(0); // declare high-level "count" state
-  return <Component {...pageProps} count={count} setCount={setCount} />; // pass it as props to the page components
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <CountContext.Provider value={count}>
+        <SetCountContext.Provider value={setCount}>
+          <Component {...pageProps} />
+        </SetCountContext.Provider>
+      </CountContext.Provider>
+    </>
+  );
 }
 
 export default MyApp;
