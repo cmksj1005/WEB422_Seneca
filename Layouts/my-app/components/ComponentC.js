bdff602c092f9017d4cb2199e301dc8ddc2730bf
Joe <cmksj1005@gmail.com>
2023-11-14 23:27:34 -0500

    update componentC for context

--- Layouts/my-app/components/ComponentC.js
+++ Layouts/my-app/components/ComponentC.js
@@ -1,7 +1,9 @@
-export default function ComponentC(props) {
+import { useContext } from 'react';
+import { SetCountContext } from '@/pages/_app';
+
+export default function ComponentC() {
+  const setCount = useContext(SetCountContext);
   return (
-    <button onClick={(e) => props.setCount((n) => n + 1)}>
-      Increase Value
-    </button>
+    <button onClick={(e) => setCount((n) => n + 1)}>Increase Value</button>
   );
 }
